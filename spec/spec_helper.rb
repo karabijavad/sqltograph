85ef62e7c921cc6efddd5795c95fa6d43eba8a5f
Javad Karabi <karabijavad@gmail.com>
2014-05-02 10:40:17 -0500

    add tests, add activerecord

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'bundler/setup'
+Bundler.setup
+
+require 'sqltograph'
+
+RSpec.configure do |config|
+end
---
+++ spec/sqltograph_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Sqltograph do
+    pending "write it"
+end
